6cb1a32a9d9e5899ac09e7289a77bf7d8f8b0273
Jean Carlos Piguave Alvarado <piguavejc@gmail.com>
2021-08-09 15:10:55 -0500

    refactorizando

---
+++ js/App.js
@@ -0,0 +1,9 @@
+import { Navegacion_movil } from "./Navegacion_movil";
+
+const navegacion_movil = new Navegacion_movil();
+navegacion_movil.obtener_abrir_menu().addEventListener("click", () => {
+  navegacion_movil.abrir();
+});
+navegacion_movil.obtener_cerrar_menu().addEventListener("click", () => {
+  navegacion_movil.cerrar();
+});
---
+++ js/Navegacion_movil.js
@@ -0,0 +1,26 @@
+class Navegacion_movil {
+  constructor() {
+    this.abrir_menu = document.getElementById("encabezado_icono_menu");
+    this.cerrar_menu = document.getElementById("cerrar_menu");
+    this.menu_movil = document.getElementById("menu_movil");
+  }
+
+  obtener_abrir_menu() {
+    return this.abrir_menu;
+  }
+  obtener_cerrar_menu() {
+    return this.cerrar_menu;
+  }
+  abrir() {
+    let css_aparece_menu = this.menu_movil.getAttribute("class");
+    css_aparece_menu += " abre";
+    this.menu_movil.setAttribute("class", css_aparece_menu);
+  }
+
+  cerrar() {
+    let css_menu_movil = this.menu_movil.getAttribute("class");
+    let css_original = css_menu_movil.split(" ");
+    this.menu_movil.setAttribute("class", css_original[0]);
+  }
+}
+export { Navegacion_movil };
