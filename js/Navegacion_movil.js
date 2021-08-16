90c0f475a04326173d65cb47f5092f85da0d00c7
Jean Carlos Piguave Alvarado <piguavejc@gmail.com>
2021-08-15 21:30:43 -0500

    refactorizando

--- js/Navegacion_movil.js
+++ js/Navegacion_movil.js
@@ -1,8 +1,8 @@
 class Navegacion_movil {
   constructor() {
-    this.abrir_menu = document.getElementById("encabezado_icono_menu");
-    this.cerrar_menu = document.getElementById("cerrar_menu");
-    this.menu_movil = document.getElementById("menu_movil");
+    this.abrir_menu = document.getElementById('encabezado_icono_menu');
+    this.cerrar_menu = document.getElementById('cerrar_menu');
+    this.menu_movil = document.getElementById('menu_movil');
   }
 
   obtener_abrir_menu() {
@@ -12,15 +12,15 @@ class Navegacion_movil {
     return this.cerrar_menu;
   }
   abrir() {
-    let css_aparece_menu = this.menu_movil.getAttribute("class");
-    css_aparece_menu += " abre";
-    this.menu_movil.setAttribute("class", css_aparece_menu);
+    let css_aparece_menu = this.menu_movil.getAttribute('class');
+    css_aparece_menu += ' abre';
+    this.menu_movil.setAttribute('class', css_aparece_menu);
   }
 
   cerrar() {
-    let css_menu_movil = this.menu_movil.getAttribute("class");
-    let css_original = css_menu_movil.split(" ");
-    this.menu_movil.setAttribute("class", css_original[0]);
+    let css_menu_movil = this.menu_movil.getAttribute('class');
+    let css_original = css_menu_movil.split(' ');
+    this.menu_movil.setAttribute('class', css_original[0]);
   }
 }
 export { Navegacion_movil };
--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,10 @@
 (() => {
-  "use strict";
+  'use strict';
   const e = new (class {
     constructor() {
-      (this.abrir_menu = document.getElementById("encabezado_icono_menu")),
-        (this.cerrar_menu = document.getElementById("cerrar_menu")),
-        (this.menu_movil = document.getElementById("menu_movil"));
+      (this.abrir_menu = document.getElementById('encabezado_icono_menu')),
+        (this.cerrar_menu = document.getElementById('cerrar_menu')),
+        (this.menu_movil = document.getElementById('menu_movil'));
     }
     obtener_abrir_menu() {
       return this.abrir_menu;
@@ -13,18 +13,18 @@
       return this.cerrar_menu;
     }
     abrir() {
-      let e = this.menu_movil.getAttribute("class");
-      (e += " abre"), this.menu_movil.setAttribute("class", e);
+      let e = this.menu_movil.getAttribute('class');
+      (e += ' abre'), this.menu_movil.setAttribute('class', e);
     }
     cerrar() {
-      let e = this.menu_movil.getAttribute("class").split(" ");
-      this.menu_movil.setAttribute("class", e[0]);
+      let e = this.menu_movil.getAttribute('class').split(' ');
+      this.menu_movil.setAttribute('class', e[0]);
     }
   })();
-  e.obtener_abrir_menu().addEventListener("click", () => {
+  e.obtener_abrir_menu().addEventListener('click', () => {
     e.abrir();
   }),
-    e.obtener_cerrar_menu().addEventListener("click", () => {
+    e.obtener_cerrar_menu().addEventListener('click', () => {
       e.cerrar();
     });
 })();
